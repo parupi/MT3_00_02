85c5862699d4458f75452667c4a58af1360335e1
Mutuking <kawaguchi.2525.haruki@gmail.com>
2024-04-18 23:53:01 +0900

    プロジェクト ファイルを追加します。

---
+++ function.cpp
@@ -0,0 +1,39 @@
+#include "function.h"
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
+{
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+Vector3 Add(const Vector3& v1, const Vector3& v2)
+{
+	return Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+Vector3 Subtract(const Vector3& v1, const Vector3& v2)
+{
+	return Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+Vector3 Multiply(float scalar, const Vector3& v)
+{
+	return Vector3(scalar * v.x, scalar * v.y, scalar * v.z);
+}
+
+float Dot(const Vector3& v1, const Vector3& v2)
+{
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+float Length(const Vector3& v)
+{
+	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+Vector3 Normalize(const Vector3& v)
+{
+	return Vector3(v.x / Length(v), v.y / Length(v), v.z / Length(v));
+}
